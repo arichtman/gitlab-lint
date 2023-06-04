cbbd8eda66453d96b7f393ca3a9213d3437a91d3
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-06-04 12:15:59 +1000

    build(dev-environment): added flake environment and updated dependencies to suit
    flakes offer a nice repeatable wrapper around poetry

---
+++ flake.nix
@@ -0,0 +1,42 @@
+# Reference https://discourse.nixos.org/t/easy-install-pypi-python-packages-mach-nix-poetry2nix/23825/6
+{
+  description = "GitLab-Lint";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    poetry2nix,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
+    (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryEnv mkPoetryApplication mkPoetryEditablePackage;
+        poetryEnv = mkPoetryEnv {
+          projectDir = ./.;
+          editablePackageSources = {
+            gitlab-lint = ./src;
+          };
+        };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+              poetryEnv
+              poetry
+            ];
+            shellHook = ''
+              pre-commit install --install-hooks
+            '';
+          };
+      }
+    );
+}
